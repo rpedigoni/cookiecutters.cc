664077825047a19012f69073b34fc4f329c11b37
Renato Pedigoni <renatopedigoni@gmail.com>
2013-12-09 09:28:18 -0200

    fixes

--- django/cookiecutters/projects/models.py
+++ django/cookiecutters/projects/models.py
@@ -18,7 +18,7 @@ class Project(models.Model):
     language = models.ForeignKey(Language, verbose_name=_('language'))
     name = models.CharField(_('name'), max_length=64)
     author = models.CharField(_('author'), max_length=64)
-    repository_url = models.CharField(_('author'), max_length=64)
+    repository_url = models.CharField(_('repository URL'), max_length=64)
     description = models.TextField(_('description'))
     created_at = models.DateTimeField(_('created at'), default=now)
 
